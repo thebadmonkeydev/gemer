db87483f79c067a3629d69d8f724a9b1545c0632
Michael Kelly <michaelkelly322@gmail.com>
2018-05-22 03:09:19 -0400

    Cover setup_config and configure methods

--- spec/gemer/configurable_spec.rb
+++ spec/gemer/configurable_spec.rb
@@ -12,4 +12,26 @@ RSpec.describe Gemer::Configurable do
   it 'should add class level `setup_config`' do
     expect(TestModule).to respond_to :setup_config
   end
+
+  context 'self#setup_config' do
+    before do
+      TestModule.setup_config do |c|
+        c.attr :test_attr, 'foo'
+        c.read_only :bar, 123
+      end
+    end
+
+    it 'should create methods on module' do
+      expect(TestModule).to respond_to :test_attr
+      expect(TestModule).to respond_to :test_attr=
+      expect(TestModule).to respond_to :bar
+    end
+
+    it 'should allow configuring values' do
+      TestModule.configure do |c|
+        expect{ c.test_attr = 'baz' }.to_not raise_error
+        expect{ c.bar = 321 }.to raise_error NoMethodError
+      end
+    end
+  end
 end
